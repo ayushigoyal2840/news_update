92240017705d790038f92b82590b54b7949dc3b4
Ayushi Goel <ayushi1911012@akgec.ac.in>
2022-02-14 19:56:26 +0530

    ,

--- newsapp/server/connection/db.js
+++ newsapp/server/connection/db.js
@@ -1,13 +1,30 @@
-import mongoose from "mongoose";
+// import mongoose from 'mongoose';
 
-const Connection = async () => {
-    try{
-  const URL=`mongodb://ayushi:Ayushi123@newsapp-shard-00-00.5fw7x.mongodb.net:27017,newsapp-shard-00-01.5fw7x.mongodb.net:27017,newsapp-shard-00-02.5fw7x.mongodb.net:27017/PROJECT 0?ssl=true&replicaSet=atlas-j040gz-shard-0&authSource=admin&retryWrites=true&w=majority`;
-  mongoose.connect(URL,{ useNewUrlParser:true });
-  console.log('database connected successfully');
-    }
-    catch (error) {
-        console.log('Error while connecting',error);
+// const Connection = async () => {
+//     try{
+//   const URL=
+//   await mongoose.connect(URL, { useNewUrlParser: true });
+//   console.log('database connected successfully');
+//     }
+//     catch (error) {
+//         console.log('Error while connecting',error);
+//     }
+// }
+
+// export default Connection;
+
+
+
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    try {   
+        const URL = `mongodb://ayushigoyal2840:Ayushi123@clone-inshort-shard-00-00.cl7sz.mongodb.net:27017,clone-inshort-shard-00-01.cl7sz.mongodb.net:27017,clone-inshort-shard-00-02.cl7sz.mongodb.net:27017/PROJECT 0?ssl=true&replicaSet=atlas-2tqdmt-shard-0&authSource=admin&retryWrites=true&w=majority`;
+        
+        
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error);
     }
 }
 
--- newsapp/server/index.js
+++ newsapp/server/index.js
@@ -6,4 +6,5 @@ const app = express();
 const PORT = 8000;
 
 Connection();
-app.listen(PORT,()=> console.log(`server is running succcessfully on ${PORT}`));
+// app.listen(PORT,()=> console.log(`server is running succcessfully on ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
